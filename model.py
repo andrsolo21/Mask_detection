2caee15f2ee97a05aa5fdd63860936a4fd34e822
andrsolo21 <andrsolo21@gmail.com>
2020-07-16 21:54:02 +0300

    start

---
+++ model.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from fastai import *
+from fastai.vision import *
+from get_image import getImage
+
+path = '.'
+learn = load_learner(path)
+
+# ret, frame = getImage()
+# print(ret)
+
